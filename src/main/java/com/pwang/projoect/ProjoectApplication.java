192c04eb393b28e402553dcd0b30cafc0162a4d5
hyunsu-hwang <hshwang1207@xinapse.ai>
2022-12-12 11:05:32 +0900

    first commit

---
+++ src/main/java/com/pwang/projoect/ProjoectApplication.java
@@ -0,0 +1,13 @@
+package com.pwang.projoect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjoectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjoectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pwang/projoect/ProjoectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwang.projoect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjoectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
